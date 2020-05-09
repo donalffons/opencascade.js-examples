cb6bd4969b7b74f5a0d35d2799aa6088235ec94d
Sebastian Alff <donalffons@gmail.com>
2020-05-09 11:25:34 +0200

    add controls

--- src/main.js
+++ src/main.js
@@ -7,22 +7,26 @@ import {
   WebGLRenderer,
 } from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 var scene = new Scene();
-var camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 var geometry = new BoxGeometry();
-var material = new MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new Mesh( geometry, material );
-scene.add( cube );
+var material = new MeshBasicMaterial({color: 0x00ff00});
+var cube = new Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
 
+new OrbitControls(camera, renderer.domElement);
+
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+	renderer.render(scene, camera);
 }
 animate();
