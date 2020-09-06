02ee8bfa4f777952cc8c79eba9e5dffa903ae2db
Sebastian Alff <donalffons@gmail.com>
2020-09-06 13:25:06 +0200

    fix deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-
-
-
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: () => {
@@ -31,19 +28,26 @@ module.exports = {
         type: "javascript/auto",
         loader: "file-loader",
         options: {
-          publicPath: "/wasm/",
+          publicPath: "../../wasm/",
           outputPath: "wasm/"
         }
       }
     ]
   },
-  plugins: fs.readdirSync(__dirname + '/src/demos').map(function(dir){
-    return new HtmlWebpackPlugin({
-      template: path.join(__dirname + '/src/demos', dir, 'index.html'),
-      filename: `demos/${dir}/index.html`,
-      chunks: [dir]
-    })
-  }),
+  plugins: [
+    ...fs.readdirSync(__dirname + '/src/demos').map(function(dir){
+      return new HtmlWebpackPlugin({
+        template: path.join(__dirname + '/src/demos', dir, 'index.html'),
+        filename: `demos/${dir}/index.html`,
+        chunks: [dir]
+      })
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/index.html', to: 'index.html' },
+      ],
+    }),
+  ],
   node: {
     fs: "empty"
   }
